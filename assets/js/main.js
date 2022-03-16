f97a4a077413775e7c78d591b544a62a82d3ec58
Moisés <guile@studiobrasuka.com.br>
2022-03-15 23:14:37 -0300

    casting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
 import mobileMenu from "./modules/MobileMenu.js";
 mobileMenu();
+
+import slidePrincipal from "./modules/MainSlide.js";
+slidePrincipal(".categoria1");
+slidePrincipal(".categoria2");
+slidePrincipal(".categoria3");
+slidePrincipal(".categoria4");
+slidePrincipal(".home_slide");
---
+++ assets/js/modules/MainSlide.js
@@ -0,0 +1,32 @@
+export default function slidePrincipal(name) {
+  var swiper = new Swiper(name, {
+    slidesPerView: 6,
+    spaceBetween: 22,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1500: {
+        slidesPerView: 6,
+      },
+      900: {
+        slidesPerView: 4,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      400: {
+        slidesPerView: 1.5,
+      },
+      200: {
+        slidesPerView: 1,
+      },
+    },
+  });
+}
